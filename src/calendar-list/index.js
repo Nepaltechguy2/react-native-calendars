2775cc03b368994fe82adff044553510296f92ca
Samyok Nepal <nepalsamyok@gmail.com>
2019-12-29 22:25:20 -0600

    added padding to top of flatlist '

--- src/calendar-list/index.js
+++ src/calendar-list/index.js
@@ -310,6 +310,9 @@ class CalendarList extends Component {
           initialScrollIndex={this.state.openDate ? this.getMonthIndex(this.state.openDate) : false}
           getItemLayout={this.getItemLayout}
           scrollsToTop={this.props.scrollsToTop}
+          style={{
+            paddingTop: 10
+          }}
         />
         {this.renderStaticHeader()}
       </View>
